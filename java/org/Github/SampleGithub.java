8071901953bf94839a6cb42d12c2a2e475e0100c
praveenshan94 <praveenlegacy7@gmail.com>
2021-08-03 15:53:19 +0530

    raj

--- java/org/Github/SampleGithub.java
+++ java/org/Github/SampleGithub.java
@@ -1,5 +1,14 @@
 package org.Github;
 
 public class SampleGithub {
+	
+	private void password() {
+		System.out.println("Password");
+		
+		public void password() {
+			System.out.println("password");
+		}
+
+	}  
 
 }
